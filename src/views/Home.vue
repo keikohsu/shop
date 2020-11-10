a368e62ca710ba9c37a2cadb954b03b5619b52c0
keikohsu <kei42568@kimo.com>
2020-11-10 13:31:57 +0800

    購物車清單

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #home
+    b-row
+      b-col(cols="12" md="6" lg="3" v-for="(item, index) in items" :key="index")
+        b-card(:title="item.name" :img-src="item.img" img-top :sub-title="'$'+item.price")
+          b-button(variant="primary" @click="addCart(item)")
+            font-awesome-icon(:icon="['fas', 'shopping-cart']")
+            | &nbsp;加入購物車
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      items: [
+        {
+          img: 'https://c.ecimg.tw/items/DXAX28A900AT004/000002_1604455767.jpg',
+          name: 'NIKE 白鞋',
+          price: 1111
+        },
+        {
+          img: 'https://c.ecimg.tw/items/DGBJDE1900AB40Y/000002_1604368410.jpg',
+          name: 'Switch',
+          price: 9580
+        },
+        {
+          img: 'https://f.ecimg.tw/items/DHAG4SA900ASCRB/000002_1603524733.jpg',
+          name: 'HP筆電',
+          price: 19111
+        },
+        {
+          img: 'https://b.ecimg.tw/items/DXAX2BA900AJR62/000002_1584518031.jpg',
+          name: 'NIKE 米色鞋',
+          price: 2911
+        }
+      ]
+    }
+  },
+  methods: {
+    addCart (item) {
+      this.$store.commit('addCart', item)
+    }
   }
 }
 </script>
